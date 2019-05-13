d25bf6a2f32b4ffa2fa340bc93b03fb3c678bde0
Desheng Li <del20@pitt.edu>
2019-05-13 13:47:51 -0400

    edit profile

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ class StageUser(db.Model):
     other_organization = db.Column(db.String(200))
     role = db.Column(db.String(100))
     other_role = db.Column(db.String(200))
-    group = db.Column(db.String(500)) # Checkboxes
+    working_group = db.Column(db.String(500)) # Checkboxes
     photo = db.Column(db.String(500))
     photo_url = db.Column(db.String(500))
     access_requests = db.Column(db.String(500)) # Checkboxes
@@ -55,7 +55,7 @@ class StageUser(db.Model):
     phone = db.Column(db.String(100))
     website = db.Column(db.String(500))
     biosketch = db.Column(db.String(200))
-    expertise = db.Column(db.String(500))
+    expertise = db.Column(db.String(1000))
     orcid = db.Column(db.String(100))
     pm = db.Column(db.Boolean)
     pm_name = db.Column(db.String(100))
@@ -73,7 +73,7 @@ class StageUser(db.Model):
             self.other_organization = a_dict['other_organization'] if 'other_organization' in a_dict else ''
             self.role = a_dict['role'] if 'role' in a_dict else ''
             self.other_role = a_dict['other_role'] if 'other_role' in a_dict else ''
-            self.group = str(a_dict['group']) if 'group' in a_dict else ''
+            self.working_group = str(a_dict['working_group']) if 'working_group' in a_dict else ''
             self.photo = a_dict['photo'] if 'photo' in a_dict else ''
             self.photo_url = a_dict['photo_url'] if 'photo_url' in a_dict else ''
             self.access_requests = str(a_dict['access_requests']) if 'access_requests' in a_dict else ''
@@ -95,7 +95,7 @@ class StageUser(db.Model):
 class StageUserSchema(ma.Schema):
     class Meta:
         fields = ('id', 'globus_user_id', 'email', 'first_name', 'last_name', 'component', 'other_component', 'organization', 'other_organization',
-                    'role', 'other_role', 'group', 'photo', 'photo_url', 'access_requests', 'google_email', 'github_username', 'slack_username', 'phone', 'website',
+                    'role', 'other_role', 'working_group', 'photo', 'photo_url', 'access_requests', 'google_email', 'github_username', 'slack_username', 'phone', 'website',
                     'biosketch', 'orcid', 'pm', 'pm_name', 'pm_email')
 
 # WPUserMeta Class/Model
@@ -287,6 +287,8 @@ def update_stage_user(stage_user_id):
         return jsonify(m_result)
     else:
         try:
+            admin_id = WPUser.query.filter(WPUser.user_login == app.config.get('ADMIN_USERNAME')).first().id
+
             new_wp_user = WPUser()
             new_wp_user.user_login = stage_user.email
             new_wp_user.user_email = stage_user.email
@@ -312,9 +314,10 @@ def update_stage_user(stage_user_id):
             photo_file_name = stage_user.photo.split('/')[-1]
             pathlib.Path(app.config.get('CONNECTION_IMAGE_PATH') + stage_user.first_name.lower() + '-' + stage_user.last_name.lower() ).mkdir(parents=True, exist_ok=True)
             copyfile(stage_user.photo, app.config.get('CONNECTION_IMAGE_PATH') + stage_user.first_name.lower() + '-' + stage_user.last_name.lower() + "/" + photo_file_name)
-            connection.options = "{\"entry\":{\"type\":\"individual\"},\"image\":{\"linked\":true,\"display\":true,\"name\":{\"original\":\"" + photo_file_name + "\"},\"meta\":{\"original\":{\"name\":\"" + photo_file_name + "\",\"path\":\"" + app.config.get('CONNECTION_IMAGE_PATH') + stage_user.globus_user_id + "\\/" + photo_file_name + "\",\"url\":\"https:\\/\\/dev2.hubmapconsortium.org\\/wp-content\\/uploads\\/connections-images\\/" + stage_user.globus_user_id + "\\/" + photo_file_name + "\",\"width\":200,\"height\":200,\"size\":\"width=\\\"200\\\" height=\\\"200\\\"\",\"mime\":\"image\\/jpeg\",\"type\":2}}}}"
+            connection.options = "{\"entry\":{\"type\":\"individual\"},\"image\":{\"linked\":true,\"display\":true,\"name\":{\"original\":\"" + photo_file_name + "\"},\"meta\":{\"original\":{\"name\":\"" + photo_file_name + "\",\"path\":\"" + app.config.get('CONNECTION_IMAGE_PATH') + stage_user.first_name.lower() + '-' + stage_user.last_name.lower() + "/" + photo_file_name + "\",\"url\": \"" + app.config.get('CONNECTION_IMAGE_URL') + stage_user.first_name.lower() + '-' + stage_user.last_name.lower() + "/" + photo_file_name + "\",\"width\":200,\"height\":200,\"size\":\"width=\\\"200\\\" height=\\\"200\\\"\",\"mime\":\"image\\/jpeg\",\"type\":2}}}}"
             connection.phone_numbers = "a:1:{i:0;a:7:{s:2:\"id\";i:417;s:4:\"type\";s:9:\"workphone\";s:4:\"name\";s:10:\"Work Phone\";s:10:\"visibility\";s:6:\"public\";s:5:\"order\";i:0;s:9:\"preferred\";b:0;s:6:\"number\";s:10:\"" + stage_user.phone + "\";}}"
-
+            
+            [db.session.delete(meta) for meta in connection.metas]
             connection_meta_component = ConnectionMeta()
             connection_meta_component.meta_key = 'component'
             connection_meta_component.meta_value = stage_user.component
@@ -338,10 +341,10 @@ def update_stage_user(stage_user_id):
             connection_meta_other_role = ConnectionMeta()
             connection_meta_other_role.meta_key = 'other_role'
             connection_meta_other_role.meta_value = stage_user.other_role
-            connection.metas.append(connection_meta_role)
+            connection.metas.append(connection_meta_other_role)
             connection_meta_working_group = ConnectionMeta()
             connection_meta_working_group.meta_key = 'working_group'
-            connection_meta_working_group.meta_value = stage_user.group
+            connection_meta_working_group.meta_value = stage_user.working_group
             connection.metas.append(connection_meta_working_group)
             connection_meta_access_requests = ConnectionMeta()
             connection_meta_access_requests.meta_key = 'access_requests'
@@ -412,9 +415,9 @@ def update_stage_user(stage_user_id):
             connection.bio = stage_user.website
             connection.notes = ''
             connection.excerpt = ''
-            connection.added_by = WPUser.query.filter(WPUser.user_login == app.config.get('ADMIN_USERNAME')).first().id
-            connection.edited_by = WPUser.query.filter(WPUser.user_login == app.config.get('ADMIN_USERNAME')).first().id
-            connection.owner = WPUser.query.filter(WPUser.user_login == app.config.get('ADMIN_USERNAME')).first().id
+            connection.added_by = admin_id
+            connection.edited_by = admin_id
+            connection.owner = admin_id
             connection.user = 0
             connection.status = 'approved'
 
