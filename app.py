5fb7c6ea187f1b7b26630b08476838345e1715be
Desheng Li <del20@pitt.edu>
2019-06-13 11:22:26 -0400

    remove bp

--- app.py
+++ app.py
@@ -703,7 +703,6 @@ def update_stage_user(stage_user_id):
         '''
         try:
             usermeta = WPUserMeta.query.filter(WPUserMeta.meta_key.like('openid-connect-generic-subject-identity'), WPUserMeta.meta_value == stage_user.globus_user_id).first()
-            import pdb; pdb.set_trace()
             if usermeta and usermeta.user:
                 wp_user = usermeta.user
             else:
