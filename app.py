bdfbb5370a7cd89e0c6c6552a312be8a009e1473
yuanzhou <yuanzhou19@gmail.com>
2019-07-19 09:47:15 -0400

    Added dir check and create if not

--- app.py
+++ app.py
@@ -591,6 +591,9 @@ def update_user_profile(connection_id, user_info, profile_pic_option, img_to_upl
     if 'image' in json.loads(connection_profile.options):
         current_image_path = json.loads(connection_profile.options)['image']['meta']['original']['path']
         current_image_filename = current_image_path.split('/')[-1]
+        # In case the image folder is gone if someone manually deleted it, we check to make and create one to avoid unknown errors
+        if not pathlib.Path(current_image_dir).exists():
+            pathlib.Path(current_image_dir).mkdir(parents=True, exist_ok=True)
     # Otherwise, this connection entry is created directly from WP connections plugin without uploading an image
     # thus there's no image folder created
     else:
